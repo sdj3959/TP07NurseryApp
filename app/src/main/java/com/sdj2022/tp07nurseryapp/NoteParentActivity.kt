5b01bba893de05413e8f2f831245608814e7d298
dj2022 <dj90996@gmail.com>
2022-11-03 16:21:49 +0900

    save

---
+++ app/src/main/java/com/sdj2022/tp07nurseryapp/NoteParentActivity.kt
@@ -0,0 +1,11 @@
+package com.sdj2022.tp07nurseryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteParentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_parent)
+    }
+}
---
+++ app/src/main/java/com/sdj2022/tp07nurseryapp/NurseryInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.sdj2022.tp07nurseryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NurseryInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nursery_info)
+    }
+}
