7db474147b26d993880ea51b0266eb1cfa5e6fbb
dj2022 <dj90996@gmail.com>
2022-11-03 15:37:59 +0900

    save

--- app/src/main/java/com/sdj2022/tp07nurseryapp/HomeFragment.kt
+++ app/src/main/java/com/sdj2022/tp07nurseryapp/HomeFragment.kt
@@ -1,17 +1,51 @@
 package com.sdj2022.tp07nurseryapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.sdj2022.tp07nurseryapp.databinding.FragmentHomeBinding
 
 class HomeFragment:Fragment() {
+
+    var binding:FragmentHomeBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.click?.setOnClickListener{ clickMenu(0) }
+        binding?.album?.setOnClickListener { clickMenu(1) }
+        binding?.note?.setOnClickListener { clickMenu(2) }
+        binding?.calendar?.setOnClickListener { clickMenu(3) }
+        binding?.foodmenu?.setOnClickListener { clickMenu(4) }
+    }
+
+    fun clickMenu(menus:Int){
+        var intent = when(menus){
+            0-> Intent(activity, NoticeActivity::class.java)
+            1-> Intent(activity, AlbumActivity::class.java)
+            2-> Intent(activity, NoteActivity::class.java)
+            3-> Intent(activity, CalendarActivity::class.java)
+            4-> Intent(activity, FoodMenuActivity::class.java)
+            else-> null
+        }
+        startActivity(intent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
--- app/src/main/java/com/sdj2022/tp07nurseryapp/MainActivity.kt
+++ app/src/main/java/com/sdj2022/tp07nurseryapp/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.sdj2022.tp07nurseryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
+import com.sdj2022.tp07nurseryapp.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     var fragments = mutableListOf<Fragment?>()
-    val fragmentManager by lazy { supportFragmentManager }
+    private val fragmentManager by lazy { supportFragmentManager }
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        // BottomNavigationView+Fragments
         fragments.add(HomeFragment())
         fragments.add(null)
         fragments.add(null)
@@ -58,5 +68,33 @@ class MainActivity : AppCompatActivity() {
 
             true
         }
+
+
+        // DrawerNavigation
+        val drawerToggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.drawer_open, R.string.drawer_close)
+        drawerToggle.syncState()
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+        binding.toolbar.setNavigationIcon(R.drawable.ic_baseline_menu_24)
+
+        val tvName =binding.nav.getHeaderView(0).findViewById<TextView>(R.id.tv_name) //DrawerNavigation headerView
+
+        binding.nav.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_logout -> {
+                    val intent = Intent(this@MainActivity, AccountActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            binding.drawerLayout.closeDrawer(binding.nav)
+
+            false
+        }
+    }//onCreate
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else onBackPressed()
     }
-}
+}//MainActivity
